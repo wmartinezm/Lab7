4b5de9af3d5313fb28b6cc6a8222e83ec92492ea
wmartinezm <wlsn.martinez@gmail.com>
2023-10-23 16:06:33 -0600

    initial update

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 #include <drivers/gpio.h>
 #include <sys/byteorder.h>
 
+void rx_callback_function(struct zcan_frame *frame, void *arg);
+
 struct zcan_frame frame = {
         .id_type = CAN_STANDARD_IDENTIFIER,
         .rtr = CAN_DATAFRAME,
@@ -14,20 +16,38 @@ struct zcan_frame frame = {
         .data = {1,2,3,4,5,6,7,8}
 };
 
+const struct zcan_filter can_filter = {
+        .id_type = CAN_STANDARD_IDENTIFIER,
+        .rtr = CAN_DATAFRAME,
+        .id = 0x124,
+        .rtr_mask = 1,
+        .id_mask = 0x7FF
+};
+
+
 const struct device *can_dev;
+struct zcan_work rx_work;
+int filter_id;
 int ret;
 
 void CAN_SendMsg(void)
 {
     ret = can_send(can_dev, &frame, K_MSEC(100), NULL, NULL);
     if (ret != CAN_TX_OK) {
-        LOG_ERR("Sending failed [%d]", ret);
-}
+        printk("Sending failed [%d]", ret);
+    }
+}   
+
+void CAN_Init(void)
+{
+    can_set_mode(can_dev, CAN_LOOPBACK_MODE);
+    filter_id = can_attach_isr(can_dev, rx_callback_function, NULL, &can_filter);
 }
 
 void main(void)
 {
     can_dev = device_get_binding("CAN_1");
+    CAN_Init();
 
     while (1)
     {
@@ -35,4 +55,10 @@ void main(void)
         k_msleep(1000);
     }
     
+}
+
+
+void rx_callback_function(struct zcan_frame *frame, void *arg)
+{
+    printk("CAN message received...");
 }
